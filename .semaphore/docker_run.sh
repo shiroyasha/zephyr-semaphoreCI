62456ba38ed68bc9ce5d22d8afb29c16ac22364e
Bogdan Milescu <bogdan.milescu@softvision.ro>
2019-11-13 22:36:25 +0200

    test docker compose

--- .semaphore/docker_run.sh
+++ .semaphore/docker_run.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-docker run --env-file env_variables \
---privileged=true \
---tty \
---net=bridge \
---user buildslave \
--v /var/run/docker.sock:/var/run/docker.sock \
+git clone https://github.com/semaphoreci/toolbox.git ~/.toolbox
+bash ~/.toolbox/install-toolbox
+source ~/.toolbox/toolbox
+
+checkout
+
+BRANCH=${SEMAPHORE_GIT_BRANCH}
+ZEPHYR_SDK_INSTALL_DIR=/opt/sdk/zephyr-sdk-0.10.3
+ZEPHYR_TOOLCHAIN_VARIANT=zephyr
+
+./scripts/ci/run_ci.sh -c -b ${BRANCH} -r origin -m ${MATRIX_BUILD} -M ${MATRIX_BUILDS};
+./scripts/ci/run_ci.sh -s -b ${BRANCH} -r origin -m ${MATRIX_BUILD} -M ${MATRIX_BUILDS};
+
