0c1ad4b13b05804eaccc987cab2dcff5f38095a4
Bogdan Milescu <bogdan.milescu@softvision.ro>
2019-11-14 06:59:58 +0200

    added PR conditions

--- .semaphore/docker_run.sh
+++ .semaphore/docker_run.sh
@@ -1,13 +1,43 @@
-#!/bin/bash
+#!/bin/bash -xe
+set -o pipefail
+
 source ~/.toolbox/toolbox
 source /tmp/.env
 
-BRANCH=${SEMAPHORE_GIT_BRANCH}
 ZEPHYR_SDK_INSTALL_DIR=/opt/sdk/zephyr-sdk-0.10.3
 ZEPHYR_TOOLCHAIN_VARIANT=zephyr
 
+if [ "${SEMAPHORE_GIT_REF_TYPE}" = "pull-request" ]; then
+    IS_PULL_REQUEST="true";
+    PULL_REQUEST_BASE_BRANCH=${SEMAPHORE_GIT_PR_BRANCH}
+else
+    IS_PULL_REQUEST="false";
+    BRANCH=${SEMAPHORE_GIT_BRANCH}
+fi;
+
 cd ${HOME}/${SEMAPHORE_PROJECT_NAME}
 
-./scripts/ci/run_ci.sh -c -b ${BRANCH} -r origin -m ${MATRIX_BUILD} -M ${MATRIX_BUILDS};
-./scripts/ci/run_ci.sh -s -b ${BRANCH} -r origin -m ${MATRIX_BUILD} -M ${MATRIX_BUILDS};
+# Build
+if [ "${IS_PULL_REQUEST}" = "true" ]; then
+    ./scripts/ci/run_ci.sh -c -b ${PULL_REQUEST_BASE_BRANCH} -r origin -m ${MATRIX_BUILD} -M ${MATRIX_BUILDS} -p ${PULL_REQUEST};
+else
+    ./scripts/ci/run_ci.sh -c -b ${BRANCH} -r origin -m ${MATRIX_BUILD} -M ${MATRIX_BUILDS};
+fi;
 
+SUCCESS=$?
+
+if [ $SUCCESS -eq 0 ]; then
+# On success
+    if [ "$IS_PULL_REQUEST" = "true" ]; then
+        ./scripts/ci/run_ci.sh -s -b ${PULL_REQUEST_BASE_BRANCH} -r origin -m ${MATRIX_BUILD} -M ${MATRIX_BUILDS} -p ${PULL_REQUEST};
+    else
+        ./scripts/ci/run_ci.sh -s -b ${BRANCH} -r origin -m ${MATRIX_BUILD} -M ${MATRIX_BUILDS};
+    fi;
+else
+# On failure
+    if [ "$IS_PULL_REQUEST" = "true" ]; then
+        ./scripts/ci/run_ci.sh -f -b ${PULL_REQUEST_BASE_BRANCH} -r origin -m ${MATRIX_BUILD} -M ${MATRIX_BUILDS} -p ${PULL_REQUEST};
+    else
+        ./scripts/ci/run_ci.sh -f -b ${BRANCH} -r origin -m ${MATRIX_BUILD} -M ${MATRIX_BUILDS};
+    fi;    
+fi
