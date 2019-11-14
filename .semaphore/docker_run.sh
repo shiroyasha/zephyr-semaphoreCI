38d6d071b4a145bd666339656de20e89e85375af
Bogdan Milescu <bogdan.milescu@softvision.ro>
2019-11-14 06:32:29 +0200

    changed docker run permissions

--- .semaphore/docker_run.sh
+++ .semaphore/docker_run.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-
-git clone https://github.com/semaphoreci/toolbox.git ~/.toolbox
-bash ~/.toolbox/install-toolbox
 source ~/.toolbox/toolbox
-
-checkout
+source /tmp/.env
 
 BRANCH=${SEMAPHORE_GIT_BRANCH}
 ZEPHYR_SDK_INSTALL_DIR=/opt/sdk/zephyr-sdk-0.10.3
 ZEPHYR_TOOLCHAIN_VARIANT=zephyr
+
+cd ${HOME}/${SEMAPHORE_PROJECT_NAME}
 
 ./scripts/ci/run_ci.sh -c -b ${BRANCH} -r origin -m ${MATRIX_BUILD} -M ${MATRIX_BUILDS};
 ./scripts/ci/run_ci.sh -s -b ${BRANCH} -r origin -m ${MATRIX_BUILD} -M ${MATRIX_BUILDS};
